21e04a57c9c7a9fa30b0a1d85485bd70ff2d3d9e
Olivia Cubela <oliviacubela@gmail.com>
2020-06-26 16:19:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,11 +21,11 @@ class Song
   end
 
   def self.genres
-    @@genres.flatten
+    @@genres.uniq
   end
 
   def self.artists
-    @@artists.flatten
+    @@artists.uniq
   end
 
   def self.genre_count
