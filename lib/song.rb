84282d1ac5d922fb032640fdc0838af1d8e99cba
Olivia Cubela <oliviacubela@gmail.com>
2020-06-26 16:21:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 3
+  @@count = 0
   @@genres = []
   @@artists = []
 
@@ -13,7 +13,7 @@ class Song
     @genre = genre
     @@genres << @genre
     @@artists << @artist
-    @@song_count +=1
+    @@count +=1
   end
 
   def self.count
